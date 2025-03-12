e7b3171566c2b3b42e5358a01feaf62e95be0701
vani0432 <grandhivani@gmail.com>
2025-03-12 16:39:15 +0530

    2nd script

---
+++ 05-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+USERNAME=$1
+PASSWORD=$2
+
+echo "username is : ${USERNAME}, password is : ${PASSWORD}"
+
+
+
+echo " enter the username"
+
+read USERNAME
+
+echo " username is : ${USERNAME}"
