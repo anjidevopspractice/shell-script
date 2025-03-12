37ec3994b162977a383cfa5e4fe42a18dac5ade8
vani0432 <grandhivani@gmail.com>
2025-03-12 16:26:46 +0530

    2nd script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hello suresh gm"
+echo "$PERSON2: hi ramesh gm"
+echo "$PERSON1: how are you suresh"
+echo "$PERSON2: i am good ramesh, how r u"
